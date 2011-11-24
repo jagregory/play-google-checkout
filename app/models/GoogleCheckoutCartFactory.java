df32844f7a8dac51a676230325c13ae783f9ab1e
James Gregory <james@jagregory.com>
2011-11-24 19:50:28 +0000

    Internalised checkout sdk
    Current callee is failing to build due to it using the sdk directly.
    Internalising it allows the parent app to only call members of the
    module.

--- app/models/GoogleCheckoutCartFactory.java
+++ app/models/GoogleCheckoutCartFactory.java
@@ -1,10 +1,9 @@
 package models;
 
 import com.google.checkout.sdk.commands.ApiContext;
-import com.google.checkout.sdk.commands.CartPoster;
 
 public final class GoogleCheckoutCartFactory {
-    public static CartPoster.CheckoutShoppingCartBuilder build(ApiContext api) {
-        return api.cartPoster().makeCart();
+    public static GoogleCheckoutCart build(ApiContext api) {
+        return new GoogleCheckoutCart(api.cartPoster().makeCart());
     }
 }
